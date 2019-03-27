ef2b79e077f1cef03fdeb4a2101e859d60c35e50
罗敏 <1455243788@qq.com>
2019-03-27 11:45:39 +0800

    乘法口诀表

---
+++ code_5.22_2.c
@@ -0,0 +1,15 @@
+//输出乘法口诀表
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%d ", i, j, i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
