b743d0552f07a1f40ab7eb64c41a5c97517e8d9b
罗敏 <1455243788@qq.com>
2019-04-12 21:31:55 +0800

    养兔子，斐波那契变形题

---
+++ day_34.cpp
@@ -0,0 +1,19 @@
+//兔子问题
+//误区：小兔子隔一天生一只，变形的斐波那契额数列
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long int a[95] = {1,2};//用数组元素解决数据过大的问题
+	int n;
+	for (int i = 2; i < 95; i++)
+	{
+		a[i] = a[i - 1] + a[i - 2];
+	}
+	while (cin >> n){
+		cout << a[n - 1] << endl;
+		
+	}
+}
+
+
