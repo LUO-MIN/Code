9cc2ea1ea92c34aa502c7fb087674a7c674e1a24
罗敏 <1455243788@qq.com>
2019-06-10 20:41:00 +0800

    汽水瓶问题解法二

---
+++ 汽水瓶问题解法二.cpp
@@ -0,0 +1,16 @@
+//三个空瓶换一瓶汽水,当有两个空瓶时可以借一个空瓶，再还
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n){
+        int sum=0;
+       
+        sum = n/2;
+        
+        cout<< sum <<endl;
+    }
+    return 0;
+}
