3207ade81c12251a09ab88be265843a729f02f20
罗敏 <1455243788@qq.com>
2019-06-22 19:49:07 +0800

    返回单链表的中间结点-快慢指针

---
+++ 返回单链表中间结点.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//快慢指针法
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(fast != NULL  && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        
+        }
+        
+        return slow;
+    }
+};
