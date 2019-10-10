266e2ea92e0492a3a7ac31217684148e0d4b414b
罗敏 <1455243788@qq.com>
2019-10-10 10:40:23 +0800

    最小的K个数

---
+++ 最小的K个数.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+        vector<int> res;
+        if(input.empty())
+        {
+            return res;
+        }
+        if(k>input.size())
+        {
+            return res;
+        }
+        sort(input.begin(), input.end());
+        for(int i=0;i<k;i++)
+        {
+            res.push_back(input[i]);
+        }
+        return res;
+    }
+};
